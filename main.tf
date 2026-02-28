3dd9a15195dc6840f2f9bc7281a0c25dea1aef64
sksinghsujit <33280324+sksinghsujit@users.noreply.github.com>
2026-03-01 00:41:35 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,40 @@
-# This doesn't actually create anything, it just outputs text
-# Perfect for testing your first "Run"
-output "hello_world" {
-  value = "Hello OpenShift!"
+terraform {
+  required_providers {
+    aap = {
+      source  = "ansible/aap"
+      version = "~> 1.4.0" # Current stable version in 2026
+    }
+  }
+}
+
+# The provider uses the variables you set in the HCP UI
+provider "aap" {
+  host                = var.AAP_HOST
+  token               = var.AAP_TOKEN
+  insecure_skip_verify = true # Set to false if you have valid SSL
+}
+
+# 1. Look up your existing Job Template by name
+data "aap_job_template" "config_app" {
+  name              = "test-jt"
+  organization_name = "Default"
+}
+
+# 2. Launch the Job
+# In AAP Provider v1.4+, we use the 'aap_job_launch' action
+resource "aap_job_launch" "run_config" {
+  job_template_id = data.aap_job_template.config_app.id
+  
+  # Pass dynamic data from Terraform to Ansible!
+  extra_vars = jsonencode({
+    target_env  = "development"
+    provisioned_by = "HCP-Terraform-Agent"
+    # Example: pass an IP from a VM you just created
+    # server_ip = aws_instance.web.private_ip 
+  })
+}
+
+# Output the Job ID so you can track it in the HCP UI logs
+output "ansible_job_id" {
+  value = aap_job_launch.run_config.job_id
 }
