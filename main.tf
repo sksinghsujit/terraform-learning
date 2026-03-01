d6dc9e86d29057842d019c5e785d5bc72fa6bec9
sksinghsujit <33280324+sksinghsujit@users.noreply.github.com>
2026-03-01 13:28:35 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,10 +21,11 @@ data "aap_job_template" "config_app" {
 }
 
 # 2. Launch the Job
-# In AAP Provider v1.4+, we use the 'aap_job_launch' action
+# In AAP Provider v1.4+, we use the 'aap_job' action
 # This needs to be validated
-resource "aap_job_launch" "run_config" {
+resource "aap_job" "run_config" {
   job_template_id = data.aap_job_template.config_app.id
+  # inventory_id = 123
   
   # Pass dynamic data from Terraform to Ansible!
   extra_vars = jsonencode({
@@ -37,5 +38,5 @@ resource "aap_job_launch" "run_config" {
 
 # Output the Job ID so you can track it in the HCP UI logs
 output "ansible_job_id" {
-  value = aap_job_launch.run_config.job_id
+  value = aap_job.run_config.job_id
 }
